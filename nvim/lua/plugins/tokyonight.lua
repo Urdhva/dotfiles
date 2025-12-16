d166c3087eb4ca3997786beafbf30073cb9ff0b4
Urdhva <urdhva.borse@gmail.com>
2025-12-16 11:45:02 +0530

    reconfigured all programs

---
+++ nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,11 @@
+return {
+enabled  = false,
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+    -- Load the colorscheme here
+    vim.cmd([[colorscheme tokyonight]])
+  end,
+}
